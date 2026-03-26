29324426433242759b8a150463856db2af23fc95
Satheesh <satheeshnarayan05@gmail.com>
2026-03-26 16:57:54 +0530

    Create enum.sv

---
+++ 01_DATA_TYPES/7_Enum/enum.sv
@@ -0,0 +1,32 @@
+module enumerated_data_types;
+  
+  enum{A,B=2,C,D=9,E,F} alpha;
+  int  i,j;
+  
+  initial begin 
+    alpha=alpha.first();
+     
+    $display("\nAlphabet and its value (first) : %s = %0d \n",alpha.name,alpha);
+    
+    
+    alpha=alpha.last();
+     
+    $display("Alphabet and its value (last) : %s = %0d \n",alpha.name(),alpha);
+    
+    for(i=0;i<alpha.num();i++)begin
+      $display("Alphabet and its value : %s = %0d ",alpha.name(),alpha);
+      alpha=alpha.prev();
+    end
+    $display("\n\n");
+    
+    alpha=alpha.first();
+    for(j=0;j<alpha.num();j++)begin
+      $display("Alphabet and its value : %s = %0d ",alpha.name(),alpha);
+      alpha=alpha.next();
+    end
+    #10; $finish;
+  end 
+
+endmodule
+  
+  
