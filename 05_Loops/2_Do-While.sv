7987c27fc5c2fe5c483a33b0fdf9a92c992a1d84
Satheesh <satheeshnarayan05@gmail.com>
2026-04-02 15:30:09 +0530

    Update 2_Do-While.sv

--- 05_Loops/2_Do-While.sv
+++ 05_Loops/2_Do-While.sv
@@ -1,1 +1,75 @@
+module do_while_loop;
+  int num;
+  
+  
+  function void sum(int n);
+    int sum;
+    int temp;
+    sum=0;
+    temp=n;
+    do begin 
+          
+      if(n>0)begin
+         sum=sum+n;
+         n--;
+        end
+      else begin
+        sum=sum+n;
+        n++;
+       end
+    end
+    while(n!=0);
+    $display("Sum of %0d numbers from 0 : %0d",temp,sum);
+    $display("\n----------------------------------\n");
+    
+  endfunction
+  
+  initial begin
+    $display("----------------------------------");
+    $display("         Do-While Loop");
+    $display("----------------------------------\n");
+    num=100;
+    $display("NUMBER : %0d",num);
+    sum(num);
+    
+    num=80;
+    $display("NUMBER : %0d",num);
+    sum(num);
+    
+    num=-10;
+    $display("NUMBER : %0d",num);
+    sum(num);
+    
+    num=-200;
+    $display("NUMBER : %0d",num);
+    sum(num);
+    
+  end 
+endmodule
+
+/*----------------------------------
+         Do-While Loop
+----------------------------------
+
+NUMBER : 100
+Sum of 100 numbers from 0 : 5050
+
+----------------------------------
+
+NUMBER : 80
+Sum of 80 numbers from 0 : 3240
+
+----------------------------------
+
+NUMBER : -10
+Sum of -10 numbers from 0 : -55
+
+----------------------------------
+
+NUMBER : -200
+Sum of -200 numbers from 0 : -20100
+
+----------------------------------
+*/
+
 
