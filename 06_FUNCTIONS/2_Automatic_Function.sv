6c785e386a54629dff737b25dcc086281e892c57
Satheesh <satheeshnarayan05@gmail.com>
2026-04-08 17:26:16 +0530

    Create 2_Automatic_Function.sv

---
+++ 06_FUNCTIONS/2_Automatic_Function.sv
@@ -0,0 +1,73 @@
+module automatic_function;
+  int num;
+  
+  
+  function automatic void sum(int n);
+    int sum;
+    int temp;
+    temp=n;
+    do begin 
+          
+      if(n>0)begin
+         sum=sum+n;
+         n--;
+        end
+      else begin
+        sum=sum+n;
+        n++;
+       end
+    end
+    while(n!=0);
+    $display("Sum of %0d numbers from 0 : %0d",temp,sum);
+    $display("\n----------------------------------\n");
+    
+  endfunction
+  
+  initial begin
+    $display("----------------------------------");
+    $display("         AUTOMATIC FUNCTION");
+    $display("----------------------------------\n");
+    num=100;
+    $display("NUMBER : %0d",num);
+    sum(num);
+    
+    num=80;
+    $display("NUMBER : %0d",num);
+    sum(num);
+    
+    num=-10;
+    $display("NUMBER : %0d",num);
+    sum(num);
+    
+    num=-200;
+    $display("NUMBER : %0d",num);
+    sum(num);
+    
+  end 
+endmodule
+/*
+----------------------------------
+         AUTOMATIC FUNCTION
+----------------------------------
+
+NUMBER : 100
+Sum of 100 numbers from 0 : 5050
+
+----------------------------------
+
+NUMBER : 80
+Sum of 80 numbers from 0 : 3240
+
+----------------------------------
+
+NUMBER : -10
+Sum of -10 numbers from 0 : -55
+
+----------------------------------
+
+NUMBER : -200
+Sum of -200 numbers from 0 : -20100
+
+----------------------------------
+*/
+
