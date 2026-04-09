0dcb478ffafa7a5ac523a88e26ffcc17437a2f33
Satheesh <satheeshnarayan05@gmail.com>
2026-04-09 12:31:39 +0530

    Update 6_Calling_with_variables_that_had_default_values.sv

--- 06_FUNCTIONS/Function_Calling_Methods/6_Calling_with_variables_that_had_default_values.sv
+++ 06_FUNCTIONS/Function_Calling_Methods/6_Calling_with_variables_that_had_default_values.sv
@@ -1,1 +1,125 @@
 
+module call_by_Variable_with_positional_arguments;
+  
+  logic [8:0] data;
+  bit parity; //parity=0 --> even parity
+              //parity=1 --> odd parity
+  
+  function automatic logic [8:0] odd_parity_generated_data(logic [8:0] d,bit p=1);
+      data={~^data,data[7:0]}; //odd parity
+    return data;
+  endfunction
+    
+    function automatic logic [8:0] even_parity_generated_data(logic [8:0] d,bit p=0);
+      data={^data,data[7:0]}; //even parity
+    return data;
+      
+  endfunction
+  
+  function automatic void data_display(ref logic [8:0] d,bit p);
+    
+    $display("Input data : %b",data);
+    
+    if(parity)begin
+      $display("\nODD Parity added Data");
+      $display("Data = %b",odd_parity_generated_data(.d(d)));   //calling the function
+    end
+    else begin
+      $display("\nEVEN Parity added Data");
+      $display("Data = %b",even_parity_generated_data(.d(d)));   //calling the function
+    end
+    
+    
+    $display("\n-----------------------------------------------\n");
+  endfunction
+    
+  initial begin 
+    
+    $display("-----------------------------------------------");
+    $display("   CALL A FUNCTION WHICH HAD DEFAULT VALUES     ");
+    $display("-----------------------------------------------");
+    data=8; parity=0;
+    data_display(.d(data),.p(parity));
+    
+    
+    data=20; parity=0;
+    data_display(.d(data),.p(parity));
+    
+    
+    data=40; parity=1;
+    data_display(.d(data),.p(parity));
+    
+    
+    data=27; parity=1;
+    data_display(.d(data),.p(parity));
+    
+    
+    data=40; parity=0;
+    data_display(.d(data),.p(parity));
+    
+    
+    data=100; parity=1;
+    data_display(.d(data),.p(parity));
+    
+    
+    data=39; parity=0;
+    data_display(.d(data),.p(parity));
+    
+  end
+    
+endmodule
+
+/*
+----------------------------------------------
+   CALL A FUNCTION WHICH HAD DEFAULT VALUES     
+-----------------------------------------------
+Input data : 000001000
+
+EVEN Parity added Data
+Data = 100001000
+
+-----------------------------------------------
+
+Input data : 000010100
+
+EVEN Parity added Data
+Data = 000010100
+
+-----------------------------------------------
+
+Input data : 000101000
+
+ODD Parity added Data
+Data = 100101000
+
+-----------------------------------------------
+
+Input data : 000011011
+
+ODD Parity added Data
+Data = 100011011
+
+-----------------------------------------------
+
+Input data : 000101000
+
+EVEN Parity added Data
+Data = 000101000
+
+-----------------------------------------------
+
+Input data : 001100100
+
+ODD Parity added Data
+Data = 001100100
+
+-----------------------------------------------
+
+Input data : 000100111
+
+EVEN Parity added Data
+Data = 000100111
+
+-----------------------------------------------
+*/
+
