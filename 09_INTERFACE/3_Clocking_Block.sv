9eaf289e0a0d18e12b302fbfd0446309aa085dc7
Satheesh <satheeshnarayan05@gmail.com>
2026-04-13 18:58:05 +0530

    Create 3_Clocking_Block.sv

---
+++ 09_INTERFACE/3_Clocking_Block.sv
@@ -0,0 +1,141 @@
+module top;
+  
+  
+  logic clk;
+  initial clk=0;
+  always #5 clk=~clk;
+  
+  port P(clk);  //INterface instantiation
+  defparam P.N=5;
+  
+  
+  parity_gen_FSM fsm(P);  //DUT instantiation
+  
+  parity_gen_FSM_Tb fsmtb(P);  //Testbench instantiation
+  
+endmodule
+//======================================
+//              INTERFACE
+//======================================
+interface port #(parameter N)(input clk);
+  
+  logic rst;
+  logic [N-1:0] x;
+  logic odd,even;
+  
+  clocking CB @(posedge clk);   
+    default input #1step output #0;   
+    output rst,x;   
+    input odd,even;    
+  endclocking  
+  
+  modport dut(input rst,input x,input clk,output odd,output even);
+  modport tb(clocking CB,input clk,output rst,output x,input odd,input even);
+  
+
+endinterface
+
+//======================================
+//                 DUT
+//======================================
+//moore
+module parity_gen_FSM(port.dut P);
+  logic [1:0] state,next;
+  localparam s0=0, //even number of ones here
+             s1=1; //odd number of ones here
+  
+  logic exor;
+  
+  always@(*)begin
+    exor=0;
+    foreach(P.x[i])begin
+      exor=exor^P.x[i];
+    end
+      
+  end
+  
+  always_ff @(posedge P.clk or posedge P.rst)begin
+    if(P.rst)
+      state<=s0;
+    else
+      state<=next;
+  end
+  
+  always_comb begin
+    next=state;
+    P.odd=0;
+    P.even=0;
+    case(state)
+      s0:begin 
+        next=(exor)?s1:s0;
+        P.odd=0;
+        P.even=1;
+      end
+      s1:begin 
+        next=(exor)?s1:s0;
+        P.odd=1;
+        P.even=0;
+      end
+      default:next=s0;
+    endcase
+    
+  end
+endmodule
+
+//======================================
+//              TESTBENCH
+//======================================
+//Testbench
+module parity_gen_FSM_Tb(port.tb P);
+  
+  
+  initial begin
+    P.CB.rst<=1; P.CB.x<=0;
+    $monitor($time,"Data_in=%b   clk=%b   rst=%b   even_parity=%b  odd_parity=%b",P.x,P.clk,P.rst,P.even,P.odd);
+    #5 P.CB.rst<=0; 
+
+    @(P.CB) P.CB.x<=$random;
+    @(P.CB) P.CB.x<=$random;
+    @(P.CB) P.CB.x<=$random;
+    @(P.CB) P.CB.x<=$random;
+    @(P.CB) P.CB.x<=$random;
+    @(P.CB) P.CB.x<=$random;
+    @(P.CB) P.CB.x<=$random;
+    @(P.CB) P.CB.x<=$random;
+    @(P.CB) P.CB.x<=$random;
+    #20 $finish;
+  end
+  initial begin
+    $dumpfile("p.vcd");
+    $dumpvars;
+  end
+  
+endmodule
+
+/*
+                   0Data_in=000   clk=1   rst=1   even_parity=1  odd_parity=0
+                   5Data_in=000   clk=0   rst=0   even_parity=1  odd_parity=0
+                  10Data_in=000   clk=1   rst=0   even_parity=1  odd_parity=0
+                  15Data_in=000   clk=0   rst=0   even_parity=1  odd_parity=0
+                  20Data_in=000   clk=1   rst=0   even_parity=1  odd_parity=0
+                  25Data_in=001   clk=0   rst=0   even_parity=1  odd_parity=0
+                  30Data_in=001   clk=1   rst=0   even_parity=0  odd_parity=1
+                  35Data_in=010   clk=0   rst=0   even_parity=0  odd_parity=1
+                  40Data_in=010   clk=1   rst=0   even_parity=0  odd_parity=1
+                  45Data_in=011   clk=0   rst=0   even_parity=0  odd_parity=1
+                  50Data_in=011   clk=1   rst=0   even_parity=1  odd_parity=0
+                  55Data_in=100   clk=0   rst=0   even_parity=1  odd_parity=0
+                  60Data_in=100   clk=1   rst=0   even_parity=0  odd_parity=1
+                  65Data_in=101   clk=0   rst=0   even_parity=0  odd_parity=1
+                  70Data_in=101   clk=1   rst=0   even_parity=1  odd_parity=0
+                  75Data_in=110   clk=0   rst=0   even_parity=1  odd_parity=0
+                  80Data_in=110   clk=1   rst=0   even_parity=1  odd_parity=0
+                  85Data_in=111   clk=0   rst=0   even_parity=1  odd_parity=0
+                  90Data_in=111   clk=1   rst=0   even_parity=0  odd_parity=1
+                  95Data_in=111   clk=0   rst=0   even_parity=0  odd_parity=1
+                 100Data_in=111   clk=1   rst=0   even_parity=0  odd_parity=1
+ 
+    */
+
+
+    
