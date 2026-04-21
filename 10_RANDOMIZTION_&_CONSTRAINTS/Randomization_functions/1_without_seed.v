02da9dfb09859cdaf6dc2afdd5b374babf71a25a
Satheesh <satheeshnarayan05@gmail.com>
2026-04-21 10:34:17 +0530

    Update 1_without_seed.v

--- 10_RANDOMIZTION_&_CONSTRAINTS/Randomization_functions/1_without_seed.v
+++ 10_RANDOMIZTION_&_CONSTRAINTS/Randomization_functions/1_without_seed.v
@@ -1,1 +1,24 @@
-
+module random_without_seed ;
+  byte a;
+  logic  [7:0] b,c;
+  int out;
+  initial begin
+    
+  repeat(10)begin 
+    a=$random;
+    b=$urandom;
+    c=$urandom_range(50,30);
+    
+    product(a,b,c,out);
+    $display("--------------------------------------");
+    $display("INPUTS  : A = %0d B = %0d C = %0d",a,b,c);
+    $display("PRODUCT : %0d",out);
+    $display("--------------------------------------");
+    
+  end
+    
+  end  
+  task product(byte a,input logic [7:0] b,c,output int pro);
+    pro=a*b*c;
+  endtask
+endmodule
