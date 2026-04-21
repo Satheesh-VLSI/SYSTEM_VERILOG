30ae2594b509a448185a9d27a67cd2c4a45960a2
Satheesh <satheeshnarayan05@gmail.com>
2026-04-21 11:46:26 +0530

    Update std::randomize.v

--- 10_RANDOMIZTION_&_CONSTRAINTS/3_Randomize_local_variables/std::randomize.v
+++ 10_RANDOMIZTION_&_CONSTRAINTS/3_Randomize_local_variables/std::randomize.v
@@ -1,12 +1,11 @@
 
-
 module std_randomize;
 
   class transaction;
 
     rand  logic [3:0] addr;     // random address
     randc logic [1:0] id;       // slave ID (0–3)
-
+    logic flag;
 
   endclass
 
@@ -22,12 +21,12 @@ module std_randomize;
     $display("     RANDOM VARIABLES: std::randomize");
     $display("=========================================");
     repeat(10) begin
-    std::randomize(write,data);
+      std::randomize(write,data,tr.flag);
       if (tr.randomize()) begin
         if (write)
-          $display("WRITE | ID=%0d ADDR=%0d DATA=%0d",tr.id,tr.addr,data);
+          $display("WRITE | ID=%0d ADDR=%0d DATA=%0d FLAG=%B",tr.id,tr.addr,data,tr.flag);
         else
-          $display("READ  | ID=%0d ADDR=%0d",tr.id,tr.addr);
+          $display("READ  | ID=%0d ADDR=%0d FLAG=%B",tr.id,tr.addr,tr.flag);
 
       end
       else begin
@@ -41,29 +40,30 @@ module std_randomize;
 
 endmodule
 
+
 /*
 =========================================
      RANDOM VARIABLES: std::randomize
 =========================================
-READ  | ID=2 ADDR=9
+WRITE | ID=2 ADDR=9 DATA=46 FLAG=0
 --------------------------------------
-READ  | ID=0 ADDR=11
+WRITE | ID=0 ADDR=11 DATA=4 FLAG=1
 --------------------------------------
-WRITE | ID=3 ADDR=10 DATA=141
+READ  | ID=3 ADDR=10 FLAG=1
 --------------------------------------
-WRITE | ID=1 ADDR=7 DATA=37
+READ  | ID=1 ADDR=7 FLAG=1
 --------------------------------------
-READ  | ID=2 ADDR=0
+WRITE | ID=2 ADDR=0 DATA=196 FLAG=1
 --------------------------------------
-WRITE | ID=1 ADDR=3 DATA=212
+WRITE | ID=1 ADDR=3 DATA=175 FLAG=0
 --------------------------------------
-READ  | ID=0 ADDR=10
+READ  | ID=0 ADDR=10 FLAG=1
 --------------------------------------
-WRITE | ID=3 ADDR=3 DATA=101
+READ  | ID=3 ADDR=3 FLAG=1
 --------------------------------------
-READ  | ID=1 ADDR=0
+READ  | ID=1 ADDR=0 FLAG=0
 --------------------------------------
-READ  | ID=0 ADDR=9
+WRITE | ID=0 ADDR=9 DATA=182 FLAG=1
 --------------------------------------
 */
 
