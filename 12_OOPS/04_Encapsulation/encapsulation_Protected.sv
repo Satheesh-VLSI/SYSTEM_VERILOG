cd567ac9d7d99b4d899d7853ead9e8bfd745436f
Satheesh <satheeshnarayan05@gmail.com>
2026-05-01 19:57:12 +0530

    Create encapsulation_Protected.sv

---
+++ 12_OOPS/04_Encapsulation/encapsulation_Protected.sv
@@ -0,0 +1,68 @@
+class parent;
+   protected int data;
+
+  function new(int data);
+    this.data=data;
+  endfunction
+  
+  
+  function int get_data();
+    return data;
+  endfunction
+  
+endclass
+
+class child extends parent;
+  
+  function new(int data);
+    super.new(data);
+  endfunction
+  
+  function void display();
+    $display("\nValue of data inside child from parent's local variable using get_data method: %0d\n",get_data());
+    $display("Value of data inside child from parent's local variable using just data: %0d",data);  //==>HERE THIS WILL NOT RISE EEROR AS THE CHILD INHERITS THE PROTECTED MEMBER.
+    
+  endfunction
+endclass
+
+module encapsulation();
+  initial begin
+    child c;
+    $display("===========================");
+    $display("      Encapsulation");
+    $display("===========================");
+    
+    c=new(45);
+    c.display(); 
+    c=new(5);
+    c.display(); 
+    c=new(19);
+    c.display(); 
+    c=new(0);
+    c.display();
+    
+    //$display("Data inside parent : %0d ",c.data); 
+    //This will rise error as the protected variable of parent is not accessible here
+  end
+endmodule
+
+/*
+===========================
+      Encapsulation
+===========================
+
+Value of data inside child from parent's local variable using get_data method: 45
+
+Value of data inside child from parent's local variable using just data: 45
+
+Value of data inside child from parent's local variable using get_data method: 5
+
+Value of data inside child from parent's local variable using just data: 5
+
+Value of data inside child from parent's local variable using get_data method: 19
+
+Value of data inside child from parent's local variable using just data: 19
+
+Value of data inside child from parent's local variable using get_data method: 0
+
+Value of data inside child from parent's local variable using just data: 0*/
